a2ad95a2f3f260f7fd9f248cda8b139bb38f129f
Philipp Gatzka <phil.gatzka@gmail.com>
2026-07-24 11:12:34 +0200

    Setup example

---
+++ src/test/java/io/github/pgatzka/example/ApplicationTest.java
@@ -0,0 +1,14 @@
+package io.github.pgatzka.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
